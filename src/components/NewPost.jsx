0c3a5942139a7e110cae165e2352b0aac64a4b34
Emre Sahiner <emre@workintech.com.tr>
2025-06-21 11:23:02 +0300

    adds ai api for translating message.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,11 @@
 import { useForm } from "react-hook-form";
 import { useAddPost } from "../services/mutations";
+import { GoogleGenAI } from "@google/genai";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWandSparkles } from "@fortawesome/free-solid-svg-icons";
 
 function NewPost() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, setValue, getValues } = useForm();
 
   const mutation = useAddPost();
 
@@ -11,11 +14,23 @@ function NewPost() {
     reset();
   }
 
+  async function translate() {
+    const ai = new GoogleGenAI({
+      apiKey: "AIzaSyCEVRuuy0aIR2BBQ7lT7XrW0QzC9kyF8P4",
+    });
+    const chat = ai.chats.create({ model: "gemini-2.0-flash" });
+
+    const response = await chat.sendMessage({
+      message: "Convert these text to english: " + getValues().title,
+    });
+    setValue("title", response.text);
+  }
+
   if (mutation.error) return <p>{mutation.error.message}</p>;
 
   return (
     <form
-      className="h-36 w-full bg-slate-200 flex justify-end flex-wrap p-4 gap-2"
+      className="h-36 w-full bg-slate-200 flex justify-between flex-wrap p-4 gap-2"
       onSubmit={handleSubmit(submitFn)}
     >
       <textarea
@@ -25,6 +40,9 @@ function NewPost() {
         {...register("title")}
       />
       <input type="hidden" value={5} {...register("userId")} />
+      <div onClick={translate}>
+        <FontAwesomeIcon icon={faWandSparkles} />
+      </div>
       <button className="bg-blue-400 rounded-full px-6 text-white">
         {mutation.isPending ? "Saving..." : "Tweet"}
       </button>
